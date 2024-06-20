a28bd88541ee04585f3a90336f37634bf6f1113e
Miheer <119855608+Miheergautam@users.noreply.github.com>
2024-06-20 06:15:31 +0530

    added the userValidatorSchema

--- backend/middleware/schemaValidator.js
+++ backend/middleware/schemaValidator.js
@@ -11,7 +11,26 @@ const adminValidationSchema = zod.object({
   address: zod.string().min(2).max(100).optional(),
 });
 
+const userValidationSchema = zod.object({
+  firstName: zod.string().min(2).max(50).optional(),
+  lastName: zod.string().min(2).max(50).optional(),
+  email: zod.string().email().optional(),
+  password: zod.string().min(6).optional(),
+  dateOfBirth: zod.string().min(2).max(50).optional(),
+  gender: zod.string().min(2).max(10).optional(),
+  address: zod.string().min(2).max(100).optional(),
+  city: zod.string().min(2).max(50).optional(),
+  state: zod.string().min(2).max(50).optional(),
+  country: zod.string().min(2).max(50).optional(),
+});
+
+const passwordValidatorSchema = zod.object({
+  currentPasword: zod.string().min(6).max(50),
+  newPassword: zod.string().min(6).max(50),
+});
 
 module.exports = {
   adminValidationSchema,
+  userValidationSchema,
+  passwordValidatorSchema,
 };
